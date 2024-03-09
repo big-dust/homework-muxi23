e007ffff92adab6b7fddeee587381084f05ae995
caicai789525 <kevin13537412565@163.com>
2024-03-09 18:32:02 +0800

    czy

--- homework/week2/week2.go
+++ homework/week2/week2.go
@@ -1,46 +1,28 @@
 package main
 
 import (
-	"context"
 	"fmt"
 )
 
-func mergeStrings(ctx context.Context, letters, numbers string, result chan<- string) {
-	defer close(result)
-
-	var merged string
-	letterIndex, numberIndex := 0, 0
-
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Context canceled, stopping goroutine")
-			return
-		default:
-			if letterIndex < len(letters) {
-				merged += string(letters[letterIndex])
-				letterIndex++
-			}
-			if numberIndex < len(numbers) {
-				merged += string(numbers[numberIndex])
-				numberIndex++
-			}
-			result <- merged
-			merged = ""
-		}
-	}
-}
-
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	str2 := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	str1 := "0123456789"
 
-	result := make(chan string)
+	ch := make(chan rune)
 
-	go mergeStrings(ctx, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", result)
+	go func() {
+		for _, char := range str1 {
+			ch <- char
+		}
+	}()
 
-	for merged := range result {
-		fmt.Println(merged)
+	go func() {
+		for _, char := range str2 {
+			ch <- char
+		}
+	}()
+
+	for i := 0; i < len(str1)+len(str2); i++ {
+		fmt.Printf("%c", <-ch)
 	}
 }
